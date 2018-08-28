66e523d1e0fe2dbcfeb037bafe451da787eee2fc
Ahmed Abdel Gadir <ahmed.gadir@gmail.com>
2018-08-28 02:50:02 +0100

    updated using git

--- 01-Udemy-React-Complete-Guide/01-react-complete-guide/src/HOC/Auxiliary.js
+++ 01-Udemy-React-Complete-Guide/01-react-complete-guide/src/HOC/Auxiliary.js
@@ -1,4 +1,7 @@
 //HOC stands for Higher Order Component
+
+// i think this functionality is now built into React as <Fragment> 
+
 const aux = props => props.children
 
 export default aux;
--- 01-Udemy-React-Complete-Guide/01-react-complete-guide/src/HOC/hoc.js
+++
@@ -1,6 +0,0 @@
-//HOC stands for Higher Order Component
-import React from 'react';
-
-const aux = props => props.children
-
-export default aux;
