977a03df46ae3f5ef0acfec77f93304b119924e7
Ahmed Abdel Gadir <ahmed.gadir@gmail.com>
2018-09-13 05:22:57 +0100

    update

--- 01-Udemy-React-Complete-Guide/04-simple-burger-builder/src/components/Layout/Layout.js
+++ 01-Udemy-React-Complete-Guide/04-simple-burger-builder/src/components/Layout/Layout.js
@@ -7,16 +7,38 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-	<React.Fragment>
-		{/* this is a nice way of drafting things up*/}
-		<Toolbar />
-		<SideDrawer />
-		<div>SideDrawer, Backdrop</div>
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</React.Fragment>
-)
+class Layout extends React.Component {
 
-export default layout
+	state = {
+		showSideDrawer: false,
+
+	}
+
+	sideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer: false})
+	}
+
+	sideDrawerToggleHandler = () => {
+		this.setState(prevState => {
+			return {showSideDrawer: !prevState.showSideDrawer};
+		});
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				{/* this is a nice way of drafting things up*/}
+				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+				<SideDrawer 
+					closed={this.sideDrawerClosedHandler}
+					open={this.state.showSideDrawer}/>
+				<div>SideDrawer, Backdrop</div>
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</React.Fragment>
+		)
+	}
+}
+
+export default Layout
