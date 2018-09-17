adc75a2dc984045232e702a4be10c252d4f06980
Ahmed Abdel Gadir <ahmed.gadir@gmail.com>
2018-09-17 22:06:10 +0100

    update

--- 01-Udemy-React-Complete-Guide/05-http-with-axios/src/App.js
+++ 01-Udemy-React-Complete-Guide/05-http-with-axios/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
+// the --save command creates an entry in the package.json file, so we can easily share our project
+
 import Blog from './containers/Blog/Blog';
 // npm install axios --save
 
+import { BrowserRouter } from 'react-router-dom'
+//npm install --save react-router-dom
+
 class App extends Component {
   render() {
+  	// by wrapping everything in the BrowserRouter component, weve turned on routing for our app
     return (
-      <div className="App">
-        <Blog />
-      </div>
+    	<BrowserRouter>
+      		<div className="App">
+        		<Blog />
+      		</div>
+      	</BrowserRouter>
     );
   }
 }
--- 01-Udemy-React-Complete-Guide/05-http-with-axios/src/containers/Blog/Blog.js
+++ 01-Udemy-React-Complete-Guide/05-http-with-axios/src/containers/Blog/Blog.js
@@ -1,64 +1,45 @@
+// when users click on HTML link tags the page reloads
+// this causes our javascript to reload and any state in our app is lost (this is rarely what we want in single page apps)
+// so instead we use the Link component provided by React
 import React, { Component } from 'react';
-import axios from 'axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
+
+import { Route, Link } from 'react-router-dom'
+// when you introduce routing into your app, you typically turn all components into containers
+// as those containers will normally have their own subcomponents which they distribute their state to.
+
+
 
 // componentDidMount and componentDidUpdate are important when causing side effects
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false,
-    }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-           .then(response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                })
-                this.setState({posts: updatedPosts})
-            })
-           .catch(err => {
-                console.log(err)
-                this.setState({error: true})
-           })
-    }
-
-    postSelected = id => {
-        this.setState({selectedPostId: id})
-    }
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelected(post.id)}/>
-            });
-        }
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
+                            {/*another way of using the Link component: 
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true'}}>New Post</Link></li>*/}
+                        </ul>
+                    </nav>
+                </header>
+                {/* if the current path is prefixed with the path property then the jsx in the render prop is rendered,
+                to set it so that the jsx in the render prop is only rendered when your exactly on the path property, set exact to true (Default is false)*/}
+                {/*<Route path="/" exact={true} render={() => <h1>yo</h1>}/> */}
+                
+                {/*if you want to render a component, you can use the component prop*/}
+                <Route path="/" exact={true} component={Posts} />
+                <Route path="/new-post" component={NewPost} />
+
             </div>
         );
     }
