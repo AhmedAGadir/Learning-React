a9d0694a0acb7329b5200154088d45ab0685180d
Ahmed Abdel Gadir <ahmed.gadir@gmail.com>
2018-10-08 23:55:45 +0100

    update

---
+++ 01-Udemy-React-Complete-Guide/04-burger-builder-application/src/components/UI/Input/Input.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import classes from './Input.css';
+
+const input = props => {
+	console.log(props)
+	let inputElement = null;
+	const inputClasses = [classes.InputElement];
+
+	if (props.invalid && props.shouldValidate && props.touched) {
+		inputClasses.push(classes.Invalid)
+	}
+
+	switch (props.elementType) {
+		case ('input'):
+			// inputElement = <input className={classes.InputElement} {...props} />
+			inputElement = <input className={inputClasses.join(' ')} onChange={props.changed} value={props.value} {...props.elementConfig} />
+			break;
+		case ('textarea'):
+			inputElement = <textarea className={inputClasses.join(' ')} onChange={props.changed} value={props.value} {...props.elementConfig} />
+			break;
+		case ('select'): 
+			inputElement = (
+				<select className={inputClasses.join(' ')} onChange={props.changed} value={props.value}>
+					{props.elementConfig.options.map(option => (
+						<option value={option.value} key={option.value}>
+							{option.displayValue}
+						</option>
+					))}
+				</select>
+			)
+			break;
+		// can add dropdown, select etc.
+		default: 
+			inputElement = <input className={inputClasses.join(' ')} onChange={props.changed} value={props.value} {...props.elementConfig} />
+	} 
+	return (
+		<div className={inputClasses.join(' ')}>
+			<label className={classes.Label}>{props.label}</label>
+			{inputElement}
+		</div>
+	)
+}
+
+// We're not showing any error messages in our form, but you can of course easily add some.
+
+// The form inputs (<Input />  component) already receives the information whether it's invalid or not. You could of course easily add some conditionally rendered element inside of that component.
+
+// For example (inside <Input />  component function):
+
+// // all the other code from the videos
+ 
+// let validationError = null;
+// if (props.invalid && props.touched) {
+//     validationError = <p>Please enter a valid value!</p>;
+// }
+ 
+// return (
+//      <div className={classes.Input}>
+//          <label className={classes.Label}>{props.label}</label>
+//          {inputElement}
+//          {validationError}
+//      </div>
+//  );
+// This could of course be finetuned. You could also pass the value type (e.g. "email address" ) as a prop:
+
+// validationError = <p>Please enter a valid {props.valueType}</p>; 
+
+// You could also receive the complete error message as a prop:
+
+// validationError = <p>{props.errorMessage}</p>; 
+
+// And of course, also don't forget to style the messages if you want to do that:
+
+// validationError = <p className={classes.ValidationError}>{props.errorMessage}</p>;
+
+// In your CSS file, you could have:
+
+// .ValidationError {
+//     color: red;
+//     margin: 5px 0;
+// } 
+
+
+export default input
--- 01-Udemy-React-Complete-Guide/04-burger-builder-application/src/containers/Checkout/ContactData/ContactData.js
+++ 01-Udemy-React-Complete-Guide/04-burger-builder-application/src/containers/Checkout/ContactData/ContactData.js
@@ -1,38 +1,119 @@
+// undefined is always treated as false in comparison operators, however it also never evaluates to true
+
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: ''
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name'
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 3,
+					maxLength: 7
+				},
+				valid: false,
+				touched: false
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street'
+				},
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
+			},
+			postCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Zip Code'
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 3,
+					maxLength: 7
+				},
+				valid: false,
+				touched: false
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country'
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 3,
+					maxLength: 7
+				},
+				valid: false,
+				touched: false
+			},
+			email: {
+				elementType: 'mail',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Email'
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 3,
+					maxLength: 7
+				},
+				valid: false,
+				touched: false
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{value: 'fastest', displayValue: 'Fastest'},
+						{value: 'cheapest', displayValue: 'Cheapest'}
+					]
+				},
+				value: 'fastest',
+				validation: {},
+				valid: true,
+			}
 		},
+		formIsValid: false,
 		loading: false,
 	}
 
 	orderHandler = e => {
 		e.preventDefault();
 		this.setState({loading: true})
+		// how to copy objects
+		const formData = {};
+		for (let formElementIdentifier in this.state.orderForm) {
+			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+		}
 		const order = {
 			ingredients: this.props.ingredients,
 			// in a real app you would recalculate the price on the server. 
 			// if you dont users could potentially manipulate the price before the data gets sent
 			price: this.props.price,
-			customer: {
-				name: 'Ahmed AG',
-				address: {
-					street: 'bob street',
-					postCode: 'W1 2BB',
-					country: 'UK'
-				},
-				email: 'test@bob.com'
-			},
-			deliveryMethod: 'fastest'
+			order: formData,
 		}
 		// the baseURL is in our axios-orders.js file
 		// firebase will create a new node 'order'
@@ -50,14 +131,69 @@ class ContactData extends Component {
 			})
 	}
 
+	inputChangedHandler = (e, inputIdentifier) => {
+		// since an object is nested inside of this.state.orderForm, its not enough to just use the spread operator
+		// the reason being that the spread operator will create a shallow copy of this.state.orderForm but not a deep copy
+		// a shallow copy will copy all things like strings, numbers etc but not objects nested inside of the object were copying
+		// to create a deep copy we do the following
+		const updatedOrderForm = {
+			...this.state.orderForm
+		}
+		// we basically access and copy the property-value pair that contains the object that needs to be deep copied
+		const updatedFormElement = {...updatedOrderForm[inputIdentifier]}
+		updatedFormElement.touched = true;
+		updatedFormElement.value = e.target.value;
+		updatedFormElement.valid = this.checkValidaty(updatedFormElement.value, updatedFormElement.validation)
+		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		
+		let formIsValid = true;
+		for (let inputIdentifier in updatedOrderForm) {
+			formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+		}
+
+		this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid})
+	}
+
+	checkValidaty(value, rules) {
+		let isValid = true;
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength  && isValid;
+		}
+
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength  && isValid;
+		}
+		// etc.
+
+		return isValid;
+	}
+
 	render() {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key]
+			})
+		}
 		let form = (
-			<form>
-				<input type="text" name="name" placeholder="your name" />
-				<input type="text" name="email" placeholder="your email" />
-				<input type="text" name="street" placeholder="street" />
-				<input type="text" name="postal" placeholder="postal code" />
-				<Button btnType="Success" clicked={this.orderHandler}>Order</Button>
+			<form onSubmit={this.orderHandler}>
+				{formElementsArray.map(formElement => (
+					<Input 
+						changed={e => this.inputChangedHandler(e, formElement.id)}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+						invalid={!formElement.config.valid}
+						shouldValidate={formElement.config.validation}
+						touched={formElement.config.touched}
+						key={formElement.id} />
+				))}
+				<Button btnType="Success" disabled={!this.state.formIsValid}>Order</Button>
 			</form>
 		);
 		if (this.state.loading) {
